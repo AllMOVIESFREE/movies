d8fcf8dea811192fea3ce9a694189e6581c966be
AllMOVIESFREE <113302268+AllMOVIESFREE@users.noreply.github.com>
2025-02-26 02:19:59 +0530

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,8 +44,8 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot")],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")]
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/Movies_Webseries_Animes_bot")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/PREMIUM_MOVIES_WEB")]
             ]
         )
 
